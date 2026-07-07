0ade5e7eed89b4a580ffa9a97861226ba092603b
thanmayiperabattula <thanmayiperabattula830@gmail.com>
2026-07-07 05:31:26 +0000

    Sync LeetCode submission Runtime - 4 ms (22.99%), Memory - 19.8 MB (16.06%)

---
+++ 1274-number-of-days-between-two-dates/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def daysBetweenDates(self, date1: str, date2: str) -> int:
+        from datetime import datetime
+        d1 = datetime.strptime(date1, "%Y-%m-%d")
+        d2 = datetime.strptime(date2, "%Y-%m-%d")
+        return abs((d2 - d1).days)
