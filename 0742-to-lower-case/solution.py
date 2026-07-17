b22d177096ec53ab2a4fc156180ca05a56346dce
thanmayiperabattula <thanmayiperabattula830@gmail.com>
2026-07-17 08:33:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (45.34%)

---
+++ 0742-to-lower-case/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
