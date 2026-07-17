bb3ad44938520d0b99866f72202c9b89a6e10ebc
thanmayiperabattula <thanmayiperabattula830@gmail.com>
2026-07-17 06:30:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19 MB (98.85%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -12,3 +12,7 @@ class Solution:
                 
         return not stack
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
